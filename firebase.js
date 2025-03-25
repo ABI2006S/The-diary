7deb36f4ff9c4f845dc68bd2512902da194a8f05
ABI2006S <aleyammachennattu@gmail.com>
2025-03-25 12:05:25 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,8 +5,7 @@ const firebaseConfig = {
     projectId: "treasured-scribbles",
     storageBucket: "treasured-scribbles.appspot.com",
     messagingSenderId: "533062052125",
-    appId: "1:533062052125:web:8c1c28d5c417c7596f59f4",
-    measurementId: "G-5S4XF2FDM4"
+    appId: "1:533062052125:web:8c1c28d5c417c7596f59f4"
 };
 
 // Initialize Firebase
